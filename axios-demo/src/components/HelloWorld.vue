828c0bc1d93c2a5538ac5adf1a22c5abffd206de
guoqing <guoqing.zyzsz@bytedance.com>
2021-09-27 21:21:07 +0800

    feat:init

--- axios-demo/src/components/HelloWorld.vue
+++ axios-demo/src/components/HelloWorld.vue
@@ -1,44 +1,97 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import axios from '../../../ts-axios/src';
+const date = new Date();
+/* 
 axios({
   method: 'get',
-  url: 'http://localhost:8080/simple/get',
+  url: 'http://localhost:8081/base/get',
   params: {
-    a: 1,
-    b: 2,
+    date,
   },
 });
 
-console.log('aaaa');
+axios({
+  method: 'get',
+  url: 'http://localhost:8081/base/get',
+  params: {
+    foo: '@:$, ',
+  },
+});
+
+axios({
+  method: 'get',
+  url: 'http://localhost:8081/base/get',
+  params: {
+    foo: 'bar',
+    baz: null,
+  },
+});
+
+axios({
+  method: 'get',
+  url: 'http://localhost:8081/base/get#hash',
+  params: {
+    foo: 'bar',
+  },
+});
+
+axios({
+  method: 'get',
+  url: 'http://localhost:8081/base/get?foo=bar',
+  params: {
+    bar: 'baz',
+  },
+}); */
+const arr = new Int32Array([21, 31]);
+axios({
+  method: 'post',
+  url: 'http://localhost:8081/base/post',
+  data: {
+    a: 1,
+    b: 2,
+  },
+}).then(res => console.log(res));
+axios({
+  method: 'post',
+  url: 'http://localhost:8081/base/post',
+  responseType: 'json',
+  data: {
+    a: 3,
+    b: 4,
+  },
+}).then(res => console.log(res));
+
 defineProps<{ msg: string }>();
 
 const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+    <p>
+      Recommended IDE setup:
+      <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+      +
+      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+    <p>See <code>README.md</code> for more information.</p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
+    <p>
+      <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
+      |
+      <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+  </div>
 </template>
 
 <style scoped>
--- end/axios-end/routes/index.js
+++ end/axios-end/routes/index.js
@@ -1,20 +1,34 @@
 const router = require('koa-router')();
 
 router.get('/simple/get', async (ctx, next) => {
-  console.log('simple-get');
   ctx.body = {
     title: 'hello axios',
   };
 });
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string';
-});
-
-router.get('/json', async (ctx, next) => {
+router.get('/base/get', async (ctx, next) => {
+  // todo 获取get请求体??
   ctx.body = {
-    title: 'koa2 json',
+    body: ctx.request.url,
   };
 });
-
+router.post('/base/post', async (ctx, next) => {
+  console.log('ctx.request.body-------', ctx.request.body);
+  ctx.body = {
+    body: { ...ctx.request.body },
+  };
+});
+router.post('/base/buffer', async (ctx, next) => {
+  let msg = [];
+  ctx.req.on('data', chunk => {
+    if (chunk) {
+      return msg.push(chunk);
+    }
+  });
+  ctx.req.on('end', () => {
+    let buf = Buffer.concat(msg);
+    ctx.body = {
+      body: buf.toJSON(),
+    };
+  });
+});
 module.exports = router;
